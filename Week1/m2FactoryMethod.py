c1aebd3efaf55d964413b5c48403b35084b935d9
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2026-06-21 13:55:51 +0530

    car and trucks

--- Week1/m2FactoryMethod.py
+++ Week1/m2FactoryMethod.py
@@ -4,3 +4,13 @@ class Vehicle(ABC):
     @abstractmethod
     def start(self):
         pass
+
+class Car(Vehicle):
+    def start(self):
+        print("Car engine started")
+
+
+class Truck(Vehicle):
+    def start(self):
+        print("Truck engine started")
+
